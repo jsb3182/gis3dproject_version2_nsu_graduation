99a1bffe24d3afa7177776384543bd6411cacf49
jsb3182 <jsbaq8231516@naver.com>
2025-12-19 20:48:00 +0900

    frontdne edit

--- fronteend/fronted/vite.config.js
+++ fronteend/fronted/vite.config.js
@@ -3,28 +3,20 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
-<<<<<<< HEAD
-=======
 import cesium from 'vite-plugin-cesium'
->>>>>>> origin/develop
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     vueDevTools(),
-<<<<<<< HEAD
-=======
     cesium(),
->>>>>>> origin/develop
   ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     },
   },
-<<<<<<< HEAD
-=======
   server: {
     host: '0.0.0.0', // ✅ 외부에서도 접속 가능하게
     port: 5173,
@@ -37,5 +29,4 @@ export default defineConfig({
       }
     }
   }
->>>>>>> origin/develop
 })
