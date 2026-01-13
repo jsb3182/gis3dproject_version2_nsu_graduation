9b8c37837975aaf099aea63518871792f5234471
jsbaq8231516@naver.com <jsbaq8231516@naver.com>
2026-01-13 16:24:55 +0900

    final cmplete page

--- fronteend/fronted/app.config.js
+++ fronteend/fronted/app.config.js
@@ -4,11 +4,11 @@ export const requestConfig = {
   // 백엔드 API 서버 (Spring, Express 등)
   baseUrl: isProduction
     ? "https://www.child119.com" // 배포용
-    : "http://localhost:8080",          // 개발용
+    : "http://localhost:8081",          // 개발용 이제 8081로 접속 지오서버
 
   // Docker 기반 AI 모델 서버 (Flask 등)
   baseUrlDocker: isProduction
     ? "https://www.child119.com" // 배포용
-    : "http://localhost:8080",          // 개발용
+    : "http://localhost:8081",          // 개발용
 
 };
